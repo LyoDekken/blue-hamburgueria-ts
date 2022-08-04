8ce611de37311a517a2f53a83aaf28ee688b0166
LyoDekken <humberto.araripe12@gmail.com>
2022-08-04 13:09:45 -0300

    feat: added assets

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -20,7 +20,7 @@ export default function Home() {
             <h1>Escolha os Sabores</h1>
           </div>
           <div>
-            <h2>Sabores</h2>    
+            <h2>Sabores</h2>
             <select name="" id="">
               <option value="1">1</option>
             </select>
@@ -35,7 +35,40 @@ export default function Home() {
         </section>
       </div>
       {/*------------------------------------------------*/}
-      <aside></aside>
+      <aside>
+        <header>
+          <h2>Pedido</h2>
+          <div>
+            <button>Comer no local</button>
+            <button>P/ viagem</button>
+            <button>Delivery</button>
+          </div>
+        </header>
+        <div>
+          <div>
+            <h3>Item</h3>
+            <h3>Qtd</h3>
+            <h3>Preço</h3>
+          </div>
+        </div>
+        <div className="checkout-card-container">
+            <div>Card</div>
+            <div>Card</div>
+            <div>Card</div>
+            <div>Card</div>
+        </div>
+        <div>
+            <div>
+              <p>Desconto</p>
+              <p>R$0,00</p>
+            </div>
+            <div>
+              <p>Sub Total</p>
+              <p>R$0,00</p>
+            </div>
+            <button>Pagar</button>
+        </div>
+      </aside>
     </div>
   );
 }
